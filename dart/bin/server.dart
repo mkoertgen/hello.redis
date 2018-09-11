3c0949bf80fa3a3cfc5b193b6453d6fe72f85a64
Marcel Körtgen <marcel.koertgen@gmail.com>
2018-09-11 17:52:35 +0200

    (dart) Refactor to GET /lookup/1.2.3.4

--- dart/bin/server.dart
+++ dart/bin/server.dart
@@ -7,36 +7,40 @@ import 'dart:math';
 Future main() async {
   var server = await HttpServer.bind(InternetAddress.loopbackIPv4, 8080);
   server.forEach((HttpRequest request) async {
-    var path = request.uri.pathSegments.first;
-    print('Called ${request.uri}');
-    if (path == 'redis') {
-      return _handleRedis(request);
+    try {
+      var paths = request.uri.pathSegments;
+      if (paths.isEmpty || paths.first != 'lookup') return _handleNotFound(request);
+      return _handleLookup(request);
+    } on Exception catch(e) {
+      print(e.toString());
+      request.response
+        ..statusCode = HttpStatus.internalServerError
+        ..write(e.toString())
+        ..close();
     }
-    return _handleNotFound(request);
   });
   print('Listening on localhost:${server.port}');
 }
 
-Future _handleRedis(HttpRequest request) async {
-  String content = await request.transform(Utf8Decoder()).join();
-  Map json = jsonDecode(content);
-  String ip = json['ip'];
-  try {
-    final parsedIP = Uri.parseIPv4Address(ip).reversed.toList();
-    final score = [0, 1, 2, 3]
-        .fold(0, (acc, elem) => acc + pow(256, elem) * parsedIP[elem]);
-    // TODO: lookup score and possible IP in Redis
-    request.response
-      ..write(jsonEncode(score))
-      ..close();
-  } on FormatException {
-    request.response
-      ..statusCode = HttpStatus.badRequest
-      ..write(jsonEncode({'error': 'Could not parse $ip as IPv4'}))
-      ..close();
-  }
+Future _handleLookup(HttpRequest request) async {
+  String ip = request.uri.pathSegments[1];
+  var redis = null; // TODO
+  var value = lookup(null, ip);
+  request.response
+    ..write(jsonEncode(value))
+    ..close();
 }
 
 void _handleNotFound(HttpRequest request) => request.response
   ..statusCode = HttpStatus.notFound
   ..close();
+
+void lookup(redis, ip) {
+  final parsedIP = Uri.parseIPv4Address(ip).reversed.toList();
+  final score = [0, 1, 2, 3]
+      .fold(0, (acc, elem) => acc + pow(256, elem) * parsedIP[elem]);
+  // TODO: lookup score and possible IP in Redis
+  return score;
+  // redis.ZRangeByScore ...
+  // redis.HGetAll ...
+}
